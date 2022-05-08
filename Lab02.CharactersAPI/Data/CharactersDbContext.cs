7c69c2ca0039da97cc357135a9b049db33ce65af
NSerytsan <nserytsan@gmail.com>
2022-05-09 02:36:14 +0300

    Add project files.

---
+++ Lab02.CharactersAPI/Data/CharactersDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lab02.CharactersAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab02.CharactersAPI.Data
+{
+    public class CharactersDbContext : DbContext
+    {
+        public virtual DbSet<Biography> Biographies { get; set; } = null!;
+
+        public CharactersDbContext(DbContextOptions<CharactersDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Lab02.CharactersAPI/Model/Biography.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab02.CharactersAPI.Model
+{
+    public class Biography
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = null!;
+    }
+}
