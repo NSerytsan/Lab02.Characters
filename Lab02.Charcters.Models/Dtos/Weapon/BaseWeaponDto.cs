ae03d318a70b2120e8d6abfb96770c71b421040e
NSerytsan <nserytsan@gmail.com>
2022-05-22 03:59:27 +0300

    fix

--- Lab02.Charcters.Models/Dtos/Weapon/BaseWeaponDto.cs
+++ Lab02.Charcters.Models/Dtos/Weapon/BaseWeaponDto.cs
@@ -9,6 +9,6 @@ public abstract class BaseWeaponDto
     [Required]
     public int Attack { get; set; }
     [Required]
-    [Range(1, int.MaxValue)]
+    [Range(1, int.MaxValue, ErrorMessage = "Виберіть тип зброї")]
     public int WeaponTypeId { get; set; }
 }
