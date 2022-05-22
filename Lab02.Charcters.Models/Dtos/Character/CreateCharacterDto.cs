68c57b6aa237fdc135912a2ef8cec13380fcf83a
NSerytsan <nserytsan@gmail.com>
2022-05-22 04:02:40 +0300

    fix

--- Lab02.Charcters.Models/Dtos/Character/CreateCharacterDto.cs
+++ Lab02.Charcters.Models/Dtos/Character/CreateCharacterDto.cs
@@ -4,6 +4,5 @@ namespace Lab02.Characters.Models.Dtos.Character;
 
 public class CreateCharacterDto : BaseCharacterDto
 {
-    [Required]
     public IEnumerable<int> SkillIds { get; set; } = null!;
 }
